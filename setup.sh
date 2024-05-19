f15275b89f6d7bab0302ff5a42d14194975832f1
shubham pandav <67508771+shubhampandav@users.noreply.github.com>
2024-05-19 15:44:48 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# Install ChromeDriver
+CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /usr/local/bin
